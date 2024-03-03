30bcc175821f0c6fb9e4111d95c4395c9f53ef0f
joecthomsen <joecthomsen@gmail.com>
2024-03-03 20:02:28 +0100

    Renamed data instance keys so its matches controller

--- schemas/dataInstance.js
+++ schemas/dataInstance.js
@@ -1,55 +1,54 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose
-
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const dataInstanceSchema = new Schema(
-    {
-        // Memory bank 205
-        operatingTime: Number,
-        startCounter: Number,
-        externalSupplyVoltage: Number,
-        externalSupplyVoltageFrequency: Number,
-        powerFactor: Number,
-        overallFailureCondition: Number,
-        externalSupplyUndervoltage: Number,
-        externalSupplyUndervoltageCounter: Number,
-        externalSupplyOvervoltage: Number,
-        externalSupplyOvervoltageCounter: Number,
-        outputPowerLimitation: Number,
-        outputPowerLimitationCounter: Number,
-        thermalDerating: Number,
-        thermalDeratingCounter: Number,
-        thermalShutdown: Number,
-        thermalShutdownCounter: Number,
-        temperature: Number,
-        outputCurrentPercent: Number,
-        // Memory bank 206
-        lightSourceStartCounterResettable: Number,
-        lightSourceStartCounter: Number,
-        lightSourceOnTimeResettable: Number,
-        lightSourceOnTime: Number,
-        lightSourceVoltage: Number,
-        lightSourceCurrent: Number,
-        lightSourceOverallFailureCondition: Number,
-        lightSourceOverallFailureConditionCounter: Number,
-        lightSourceShortCircuit: Number,
-        lightSourceShortCircuitCounter: Number,
-        lightSourceOpenCircuit: Number,
-        lightSourceOpenCircuitCounter: Number,
-        lightSourceThermalDerating: Number,
-        lightSourceThermalDeratingCounter: Number,
-        lightSourceThermalShutdown: Number,
-        lightSourceThermalShutdownCounter: Number,
-        lightSourceTemperature: Number,
-        // Memory bank 207
-        ratedMedianUsefulLifeOfLuminare: Number,
-        internalControleGearReferenceTemperature: Number,
-        ratedMedianUsefulLightSourceStarts: Number
-    },
-    {
-        timestamps: true
-    }
-)
+  {
+    // Memory bank 205
+    operating_time: Number,
+    start_counter: Number,
+    external_supply_voltage: Number,
+    external_supply_voltage_frequency: Number,
+    power_factor: Number,
+    overall_faliure_condition: Number,
+    external_supply_undervoltage: Number,
+    external_supply_undervoltage_counter: Number,
+    external_supply_overvoltage: Number,
+    external_supply_overvoltage_counter: Number,
+    output_power_limitation: Number,
+    output_power_limitation_counter: Number,
+    thermal_derating: Number,
+    thermal_derating_counter: Number,
+    thermal_shutdown: Number,
+    thermal_shutdown_counter: Number,
+    temperature: Number,
+    output_current_percent: Number,
+    // Memory bank 206
+    light_source_start_counter_resettable: Number,
+    light_source_start_counter: Number,
+    light_source_on_time_resettable: Number,
+    light_source_on_time: Number,
+    light_source_voltage: Number,
+    light_source_current: Number,
+    light_source_overall_faliure_condition: Number,
+    light_source_overall_faliure_condition_counter: Number,
+    light_source_short_circuit: Number,
+    light_source_short_circuit_counter: Number,
+    light_source_open_circuit: Number,
+    light_source_open_circuit_counter: Number,
+    light_source_thermal_derating: Number,
+    light_source_thermal_derating_counter: Number,
+    light_source_thermal_shutdown: Number,
+    light_source_thermal_shutdown_counter: Number,
+    light_source_temperature: Number,
+    // Memory bank 207
+    rated_median_usefull_life_of_luminare: Number,
+    internal_controle_gear_reference_temperature: Number,
+    rated_median_usefull_light_source_starts: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const DataInstance = mongoose.model('dataInstanceSchema', dataInstanceSchema)
-module.exports = DataInstance
+const DataInstance = mongoose.model("dataInstanceSchema", dataInstanceSchema);
+module.exports = DataInstance;
